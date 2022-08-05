2b747b79cea5f24d84309f21ef4512f69bdaafc3
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-08-06 02:34:50 +0600

    header with routes created successfully

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="w-75 mx-auto">
+            <h1 className="text-center text-primary">This is Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Login/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+    return (
+        <div className="w-75 mx-auto">
+            <h1 className="text-center text-primary">This is Login Page</h1>
+        </div>
+    );
+};
+
+export default Login;
