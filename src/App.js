a9c974b69c2b94029a4ce188adc58467059e6598
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-08-10 22:12:14 +0600

    Register created successfully

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import ChessBoard from './components/ChessBoard/ChessBoard';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login/Login';
+import Register from './components/Login/Register/Register';
 import Header from './components/Shared/Header/Header';
 
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/home" element={<Home></Home>} />
         <Route path="/chessBoard" element={<ChessBoard></ChessBoard>} />
         <Route path="/login" element={<Login></Login>} />
+        <Route path="/register" element={<Register></Register>} />
       </Routes>
     </div>
   );
